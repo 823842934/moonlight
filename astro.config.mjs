bb05287a9102eddea40fab80b6dc18cad9952374
823842934 <donibalonii@gmail.com>
2025-12-25 03:46:22 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+  output: "static",
+  build: {
+    format: "file", // <-- makes dist/404.html (not dist/404/index.html)
+  },
+  trailingSlash: "never",
 });
